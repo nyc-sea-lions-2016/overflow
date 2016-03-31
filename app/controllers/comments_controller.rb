78cda9e3aa5d6d912bcdc7bc90fd7c8de91a52d6
Jonathan Case & Brett Ripley <pair+Case104.drumguy16@devbootcamp.com>
2016-03-31 17:54:28 -0400

    Saving for party time

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,10 @@
 post '/questions/:id/comment' do
-  erb :'/comments/_new_question'
+  binding.pry
+  current_user
+  @question = Question.find_by(id: params[:id])
+  @comment = @question.comments.build(body: params['body'], user_id: @user.id)
+  @comment.save
+  erb :'/comments/_show', locals: {comment: @comment}
 end
 
 post '/answers/:id/comment' do

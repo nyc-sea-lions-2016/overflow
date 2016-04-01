a17a240924a7bdbade8cae4186b26350750f9c5e
Jonathan Case & Brett Ripley <pair+Case104.drumguy16@devbootcamp.com>
2016-04-01 17:51:13 -0400

    Refactored comments. All functionality working as intended. Moving on to vote logic.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -4,3 +4,13 @@ post '/answers' do
   redirect "/questions/#{params[:questionid]}"
 end
 
+post '/answers/:id/votes' do
+  current_user
+  @answer = Answer.find_by(id: params[:id])
+  @vote = @answer.votes.build(user_id: @user.id)
+  if @vote.save
+    {new_vote_count: @answer.votes.count}.to_json
+  else
+    @errors = ["You've already voted on this"]
+  end
+end
